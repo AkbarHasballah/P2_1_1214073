57f3a4f5274b0ff68d136c87d9f4b026a3911326
AkbarHasballah <salmanakbartafisa@gmail.com>
2022-09-26 16:36:00 +0700

    percobaan 1

--- P2_1_1214073/Program.cs
+++ P2_1_1214073/Program.cs
@@ -10,7 +10,7 @@ namespace P2_1_1214073
     {
         static void Main(string[] args)
         {
-            //Tugas oertama (percobaan 1.1) aritmathic operators
+            //Tugas oertama (percobaan 1.1) aritmathic operator
            int angkaPertama = int.Parse(Console.ReadLine());
             int angkaKedua = int.Parse(Console.ReadLine());
             int a = angkaPertama + angkaKedua;
