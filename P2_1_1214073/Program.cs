5ea0cea5cc35bb895b5134f91be851d9f44071cb
AkbarHasballah <salmanakbartafisa@gmail.com>
2022-09-26 16:37:14 +0700

    perubahan 2

--- P2_1_1214073/Program.cs
+++ P2_1_1214073/Program.cs
@@ -10,7 +10,8 @@ namespace P2_1_1214073
     {
         static void Main(string[] args)
         {
-            //Tugas oertama (percobaan 1.1) aritmathic operator
+            //Tugas pertama  aritmathic operator
+            //Percobaan 1.1
            int angkaPertama = int.Parse(Console.ReadLine());
             int angkaKedua = int.Parse(Console.ReadLine());
             int a = angkaPertama + angkaKedua;
